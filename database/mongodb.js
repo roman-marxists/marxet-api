1b30bc9a767b546b050f2b83f47f4789fe3924b0
Guillermo Martinez <gmartinez948@outlook.com>
2021-12-10 17:16:23 -0500

    set up mongo

--- database/mongodb.js
+++ database/mongodb.js
@@ -1,3 +1,10 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const User = require("../models/User");
+console.log(process.env.DATABASE_URL);
+const connectDb = () => {
+  return mongoose.connect(process.env.DATABASE_URL);
+};
 
-module.exports = {};
+const models = { User };
+
+module.exports = { connectDb, models };
--- server/index.js
+++ server/index.js
@@ -1,16 +1,23 @@
-const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
-const { pgsql, mongodb } = require('../database')
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+require("dotenv").config();
+const { pgsql, mongodb } = require("../database");
+
+const { connectDb, models } = require("../database/mongodb");
 
 const port = process.env.PORT || 3001;
 
-let app = express()
+let app = express();
 
-app.use(cors())
-app.use(morgan('dev'))
-app.use(express.json())
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
 
-app.listen(port, () => {
-  console.log("Listening on port %d...", port)
-})
+connectDb()
+  .then(() => {
+    app.listen(port, () => {
+      console.log("Listening on port %d...", port);
+    });
+  })
+  .catch((err) => console.log(err));
