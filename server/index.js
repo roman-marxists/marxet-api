4fe2008c450ba78e7138f8eb175040802692f30d
Michael Christenson <mchristenson5891@gmail.com>
2021-12-11 19:03:47 -0800

    checkout main

--- server/index.js
+++ server/index.js
@@ -1,16 +1,16 @@
-const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
-const { pgsql, mongodb } = require('../database')
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const { pgsql, mongodb } = require('../database');
 
-const port = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 
-let app = express()
+let app = express();
 
-app.use(cors())
-app.use(morgan('dev'))
-app.use(express.json())
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
 
-app.listen(port, () => {
-  console.log("Listening on port %d...", port)
-})
+app.listen(PORT, (err) => {
+  console.log(err ? err : `Listening on port %d...${PORT}`);
+});
