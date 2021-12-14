1445602bf9686ef38d79f91b24599dcf95df4368
Arun Bhardwaj <arun.b.95@gmail.com>
2021-12-14 16:12:32 -0500

    refactored environment variable DATABASE_URL to MONGO_URL

--- database/mongodb.js
+++ database/mongodb.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 const { User, Item } = require('../models/User');
 const Product = require('../models/Product');
 
-console.log(process.env.DATABASE_URL);
+console.log(process.env.MONGO_URL);
 const connectDb = () => {
-  return mongoose.connect(process.env.DATABASE_URL);
+  return mongoose.connect(process.env.MONGO_URL);
 };
 
 const models = { User, Item, Product };
