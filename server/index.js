03547e965830363ad3bb861e6c83e94908999e35
Guillermo Martinez <gmartinez948@outlook.com>
2021-12-14 16:33:03 -0500

    merge main

--- server/index.js
+++ server/index.js
@@ -1,21 +1,21 @@
-const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors');
-require('dotenv').config();
-const { pgsql, mongodb } = require('../database');
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+require("dotenv").config();
+const { pgsql, mongodb } = require("../database");
 
-const { connectDb, models } = require('../database/mongodb');
+const { connectDb, models } = require("../database/mongodb");
 
 const PORT = process.env.PORT || 3001;
 
 let app = express();
 
 app.use(cors());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.json());
 
-app.get('/api/fake', async (req, res) => {
-  const user = await models.User.find({ username: 'hellothere' });
+app.get("/api/fake", async (req, res) => {
+  const user = await models.User.find({ username: "hellothere" });
 
   // const user = await models.User.create({
   //   username: "hellothere",
@@ -31,8 +31,8 @@ app.get('/api/fake', async (req, res) => {
 
 connectDb()
   .then(() => {
-    app.listen(port, () => {
-      console.log('Listening on port %d...', port);
+    app.listen(PORT, () => {
+      console.log("Listening on port %d...", PORT);
     });
   })
   .catch((err) => console.log(err));
