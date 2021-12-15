ee01594ac4d5a158348e6b26bf5b2e9d10b7203f
Arun Bhardwaj <arun.b.95@gmail.com>
2021-12-14 21:48:50 -0500

    adds db connection to export and removes Item model references

--- database/mongodb.js
+++ database/mongodb.js
@@ -1,13 +1,17 @@
 const mongoose = require("mongoose");
-const { User, Item } = require("../models/User");
+const { User } = require("../models/User");
 const Product = require("../models/Product");
 const Category = require("../models/Category");
 
-console.log(process.env.MONGO_URL);
+
 const connectDb = () => {
   return mongoose.connect(process.env.MONGO_URL);
 };
 
-const models = { User, Item, Product, Category };
+const db = mongoose.connection;
 
-module.exports = { connectDb, models };
+console.log("🚀 ~ file: mongodb.js ~ line 11 ~ db", db)
+
+const models = { User, Product, Category };
+
+module.exports = { db, connectDb, models };
