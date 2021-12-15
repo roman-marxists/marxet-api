630326ccafdea1782781bd471a624eb1f6735496
Arun Bhardwaj <arun.b.95@gmail.com>
2021-12-15 11:39:57 -0500

    adds single-document transactions

--- controllers/TransactionController.js
+++ controllers/TransactionController.js
@@ -1,47 +1,99 @@
 const { db } = require('../database/mongodb');
 const { User, Product, Transaction } = require('../models');
 
-const createTransaction = async (buyer, seller, productId) => {
+/**
+ * To be implemented as Users will have their inventories updated in addition
+ * to Transactions needing to have an entry created.
+ *
+ * @param {*} buyerId
+ * @param {*} sellerId
+ * @param {*} productId
+ * @returns
+ */
+const createTransactionReplicaSet = async (buyerId, sellerId, productId) => {
   const session = await Transaction.startSession();
 
   try {
     const transactionResults = await session.withTransaction(async () => {
-      const buyerUser = await User.findById(buyer.id);
-      const sellerUser = await User.findById(seller.id);
-      const listing = await Product.findById(productId);
+      const buyerUser = await User.findById(buyerId).session(session);
+      const sellerUser = await User.findById(sellerId).session(session);
+      const listing = await Product.findById(productId).session(session);
 
       const isListingAlreadySold = await Transaction.findOne({ product: listing }).session(session);
       if (isListingAlreadySold) {
-        console.error('This listing is already sold. The transaction could not take place.');
-        return session.abortTransaction();
+        await session.abortTransaction();
+        throw new Error('This listing is already sold. The transaction could not take place.');
       }
 
-      const transactionUpdateResults = await Transaction.create([{
-        seller: sellerUser,
-        buyer: buyerUser,
-        product: listing,
-      }], { session: session });
-      console.log(`${transactionUpdateResults}`)
+      // Create the transaction
+      const transactionUpdateResults = await Transaction.create(
+        [
+          {
+            seller: sellerUser,
+            buyer: buyerUser,
+            product: listing,
+          },
+        ],
+        { session: session }
+      );
+      if (transactionUpdateResults?.modifiedCount !== 1) {
+        await session.abortTransaction();
+        throw new Error('This transaction was not completed.');
+      }
 
+      // remove the listing from user inventory.
+      const deleteFromUserInventoryResults = await User.findById(listing._id).populate().session(session);
+      if (deleteFromUserInventoryResults?.deletedCount !== 1) {
+        await session.abortTransaction();
+        throw new Error('This transaction was not completed because the listing could not be removed.');
+      }
 
+      return transactionUpdateResults;
     });
 
+    console.log(transactionResults);
     if (transactionResults) {
       console.log('Transaction was completed successfully.');
     } else {
-      console.log("🚀 ~ file: TransactionController.js ~ line 33 ~ createTransaction ~ transactionResults", transactionResults)
       console.log('Transaction was aborted.');
     }
   } catch (e) {
-    console.log("The transaction was aborted due to an unexpected error: " + e);
+    console.log('The transaction was aborted due to an unexpected error: ' + e);
   } finally {
     await session.endSession();
   }
 };
 
+const createTransaction = async (buyerId, sellerId, productId) => {
+  const buyerUser = await User.findById(buyerId).exec();
+  const sellerUser = await User.findById(sellerId).exec();
+  const listing = await Product.findById(productId).exec();
 
-const buyer1 = {id: 1}
-const seller1 = {id: 2}
+  const isListingAlreadySold = await Transaction.findOne({ product: listing }).exec();
+  if (isListingAlreadySold) {
+    throw new Error('This listing is already sold. The transaction could not take place.');
+  }
+
+  if (!buyerUser || !sellerUser) {
+    throw new Error('The buyer or seller could not be found.');
+  }
+
+  return Transaction.create({
+    seller: sellerUser,
+    buyer: buyerUser,
+    product: listing,
+  });
+};
+
+// Tests
+const buyer1 = 1;
+const seller1 = 2;
 const saleProduct = '61b969652af66bf11d6d550b';
 
-createTransaction(buyer1, seller1, saleProduct);
+createTransaction(buyer1, seller1, saleProduct)
+  .then(() => {
+    console.log('Completed succesfully');
+  })
+  .catch(err => {
+    console.log('Sorry, the transaction was not completed: ' + err);
+  });
